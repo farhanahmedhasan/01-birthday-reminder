5e3d946678f0ebae4a28631814b5223423eaa801
Hasan <aaxhasandjmamunxyz2015@gmail.com>
2021-05-14 01:03:03 +0600

    Simple Birthday Reminder

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
+function App() {
+  const [people, setPeople] = useState(data);
+
+  const clearPeople = () => {
+    setPeople([]);
+  };
+
+  return (
+    <main>
+      <section className='container'>
+        <h2>{people.length} Birthdays Today</h2>
+        <List people={people} />
+        <button type='button' onClick={clearPeople}>
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
